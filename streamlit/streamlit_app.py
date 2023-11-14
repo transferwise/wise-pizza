4319f93e772cbe3ebc1216a4c4767d6dc918642a
agusfigueroa-htg <agustin.figueroa@hometogo.de>
2023-11-14 15:49:25 +0100

    chore: add comments

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -27,6 +27,7 @@ st.set_page_config(layout="wide", page_title="Wise Pizza", page_icon=":pizza:")
 st.title('Wise Pizza powered by Streamlit')
 st.text('Only categorical columns are accepted, bucket the numeric ones if you wanna use those')
 
+# upload the file from the computer
 def load_data_upload():
     uploaded_file = st.file_uploader("Choose a file")
     data = pd.read_csv(uploaded_file)
@@ -35,15 +36,18 @@ def load_data_upload():
 on = st.toggle('Use sample data from Github')
 url_data = (r'https://raw.githubusercontent.com/transferwise/wise-pizza/main/data/synth_data.csv')
 
+# select the datasource, either local or from github
 if on:
     st.write(f'Downloading data from {url_data}!')
     df = pd.read_csv(url_data)
 else:
     df=load_data_upload()
 
+# show dataset preview
 st.text('Table preview')
 st.table(df.head(10))
 
+# ask the user via streamlit for the target column
 totals = st.selectbox(
    "What is the target column that you want to analyse? e.g. GMV/revenue",
    df.columns,
@@ -52,7 +56,7 @@ totals = st.selectbox(
 )
 st.write('You selected:', totals)
 
-
+# ask the user via streamlit for the size column
 size = st.selectbox(
    "What is the volume column of your dataset? e.g. number of users/transactions",
    df.columns,
@@ -61,7 +65,7 @@ size = st.selectbox(
 )
 st.write('You selected:', size)
 
-
+# ask the user via streamlit for the flag column
 flag_column = st.selectbox(
    "What is the flag column of your dataset you wanan split it by? Ensure this column is binary",
    df.columns,
@@ -70,32 +74,41 @@ flag_column = st.selectbox(
 )
 st.write('You selected:', flag_column)
 
-flags = sorted(df[flag_column].unique())  # unique flags in the dataset
+# calculate unique flags in the dataset
+flags = sorted(df[flag_column].unique())  
 
+# show an error message if the specified flag column is not binary
 if len(flags)>2:
     st.error('Your flag is not binary', icon="🚨")
 
+# allow users to define what's "old" and "new" in the comparison
 flags_option = st.selectbox(
     'Which one in your data belongs to group A?',
     (flags))
 
+# listing all potential dimensions in the dataframe. 
+# all of them are potential columns to exclude
 candidates_excluded_columns = [element for element in df.columns if element not in [totals,size,flag_column]]
 
+# list with specified columns to exclude.
 excluded_columns = st.multiselect(
     'Please select all columns that you want to exclude from the analysis',
     candidates_excluded_columns)
 
+# all non dimensional columns are the ones picked by the user (if any), plus the ones that indicate totals, size and flag
 non_dimensional_columns = excluded_columns + [totals,size,flag_column]
 
+# calculating dimensions
 dims = [element for element in df.columns if element not in non_dimensional_columns]
 
+# creating the dataframes for the comparison calculations
 data = df[df[flag_column] != flags_option]  # take the group to compare to
 pre_data = df[df[flag_column] == flags_option]  # take the group to be compared
 
 st.subheader('Finding the juiciest slices', divider='rainbow')
 st.text('This section does not compare groups, but rather checks which features have the most impact in the target column you selected.')
 
-##Finding juiciest slices
+## finding juiciest slices
 sf = explain_levels(
     df=df,
     dims=dims,
@@ -107,13 +120,15 @@ sf = explain_levels(
     return_fig=True
 )
 
+# storing the plot in a variable
 plot_sf=sf.plot(width=500, height=500)
+# exposing the plot via streamlit
 st.plotly_chart(plot_sf, use_container_width=True)
 
 st.subheader('Analysing differences', divider='rainbow')
 st.text('This section does compare the two groups defined by the flag. Old total is the group A you selected in the dropdown')
 
-##explaining changes overall
+## running explain calculations
 sf1 = explain_changes_in_totals(
     df1=pre_data,
     df2=data,
@@ -126,8 +141,10 @@ sf1 = explain_changes_in_totals(
     solver="lasso",
     return_fig=True
 )
+# specifying a two column layout on streamlit
 col1, col2 = st.columns(2)
-
+# storing the plots in variables
+# exposing the plots via streamlit
 with col1:
     plot_sf1=sf1.plot(width=500, height=500, plot_is_static=plot_is_static)[0]
     st.plotly_chart(plot_sf1, use_container_width=True)
@@ -139,6 +156,7 @@ with col2:
 st.subheader('Decomposing differences', divider='rainbow')
 st.text('`split_fits` to separately decompose contribution of size changes and average changes')
 
+## running explain calculations
 sf2 = explain_changes_in_totals(
     df1=pre_data,
     df2=data,
@@ -151,10 +169,14 @@ sf2 = explain_changes_in_totals(
     solver="lasso",
     return_fig=True
 )
+# storing the plot in a variable
+# exposing the plot via streamlit
 plot_sf=sf2.plot(width=500, height=500)
 st.plotly_chart(plot_sf, use_container_width=True)
 
 st.text('`extra_dim` to treat size vs average change contribution as an additional dimension')
+
+## running explain calculations
 sf3 = explain_changes_in_totals(
     df1=pre_data,
     df2=data,
@@ -167,9 +189,11 @@ sf3 = explain_changes_in_totals(
     solver="lasso",
     return_fig=True
 )
-
+# specifying a two column layout on streamlit
 col1, col2 = st.columns(2)
 
+# storing the plots in variables
+# exposing the plots via streamlit
 with col1:
     plot_sf1=sf3.plot(width=500, height=500, plot_is_static=plot_is_static)[0]
     st.plotly_chart(plot_sf1, use_container_width=True)
@@ -179,6 +203,8 @@ with col2:
     st.plotly_chart(plot_sf2, use_container_width=True)
 
 st.text('`force_dim` like extra_dim, but each segment must contain a Change_from constraint')
+
+## running explain calculations
 sf3 = explain_changes_in_totals(
     df1=pre_data,
     df2=data,
@@ -191,8 +217,11 @@ sf3 = explain_changes_in_totals(
     solver="lasso",
     return_fig=True
 )
+# specifying a two column layout on streamlit
 col1, col2 = st.columns(2)
 
+# storing the plots in variables
+# exposing the plots via streamlit
 with col1:
     plot_sf1=sf3.plot(width=500, height=500, plot_is_static=plot_is_static)[0]
     st.plotly_chart(plot_sf1, use_container_width=True)
@@ -203,6 +232,7 @@ with col2:
 
 st.subheader('Explaining changes in average', divider='rainbow')
 
+## running explain calculations
 sf4 = explain_changes_in_average(
     df1=pre_data,
     df2=data,
@@ -215,9 +245,11 @@ sf4 = explain_changes_in_average(
     solver="lasso",
     return_fig=True
 )
-
+# specifying a two column layout on streamlit
 col1, col2 = st.columns(2)
 
+# storing the plots in variables
+# exposing the plots via streamlit
 with col1:
     plot_sf1=sf4.plot(width=500, height=500, plot_is_static=plot_is_static)[0]
     st.plotly_chart(plot_sf1, use_container_width=True)
@@ -226,6 +258,7 @@ with col2:
     plot_sf2=sf4.plot(width=500, height=500, plot_is_static=plot_is_static)[1]
     st.plotly_chart(plot_sf2, use_container_width=True)
 
+## running explain calculations
 sf6 = explain_changes_in_average(
     df1=pre_data,
     df2=data,
@@ -238,5 +271,8 @@ sf6 = explain_changes_in_average(
     solver="lasso",
     return_fig=True
 )
+
+# storing the plot in a variable
 plot_sf=sf6.plot(width=500, height=500)
+# exposing the plot via streamlit
 st.plotly_chart(plot_sf, use_container_width=True)
