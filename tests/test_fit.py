Tidy up the plotting data interface, fix nan scaling bug. Predict with identity transform works.

tests/test_fit.py
@@ -7,11 +7,16 @@ import pandas as pd
 import pytest
 
 from wise_pizza.data_sources.synthetic import synthetic_data, synthetic_ts_data
-from wise_pizza.explain import explain_changes_in_average, explain_changes_in_totals, explain_levels, explain_timeseries
+from wise_pizza.explain import (
+    explain_changes_in_average,
+    explain_changes_in_totals,
+    explain_levels,
+    explain_timeseries,
+)
 from wise_pizza.segment_data import SegmentData
 from wise_pizza.solver import solve_lasso, solve_lp
 from wise_pizza.time import create_time_basis
-from wise_pizza.plotting import plot_time
+from wise_pizza.plotting_time import plot_time
 
 np.random.seed(42)
 
@@ -101,8 +106,12 @@ def size_to_one(
 
 
 def monthly_driver_data():
-    df = pd.read_csv(os.path.join(os.path.dirname(__file__), "../data", "synth_data.csv"))
-    return SegmentData(data=df, dimensions=dims, segment_total=totals, segment_size=size)
+    df = pd.read_csv(
+        os.path.join(os.path.dirname(__file__), "../data", "synth_data.csv")
+    )
+    return SegmentData(
+        data=df, dimensions=dims, segment_total=totals, segment_size=size
+    )
 
 
 def test_categorical():
@@ -195,7 +204,6 @@ def test_synthetic_ts_template(nan_percent: float):
 
     assert abs(sf.segments[0]["coef"] - 300) < 2
     assert abs(sf.segments[1]["coef"] - 100) < 2
-
 
     # sf.plot()
     print("yay!")

wise_pizza/slicer_facades.py
@@ -1,45 +1,15 @@
+import copy
 from typing import Optional, Dict
-from abc import ABC, abstractmethod
 
 import numpy as np
 import pandas as pd
 
+from wise_pizza.slicer_plotting import SliceFinderPlottingInterface
 from wise_pizza.transform import TransformWithWeights, IdentityTransform
-from wise_pizza.plotting import plot_time
+from wise_pizza.plotting_time import plot_time
 
 
-class SliceFinderFacade(ABC):
-    @property
-    @abstractmethod
-    def actual_totals(self):
-        pass
-
-    @property
-    @abstractmethod
-    def predicted_totals(self):
-        pass
-
-    @property
-    @abstractmethod
-    def segments(self):
-        pass
-
-    @property
-    @abstractmethod
-    def time(self):
-        pass
-
-    @property
-    @abstractmethod
-    def total_name(self):
-        pass
-
-    @abstractmethod
-    def segment_impact_on_totals(self, s: Dict) -> np.ndarray:
-        pass
-
-
-class SliceFinderPredictFacade(SliceFinderFacade):
+class SliceFinderPredictFacade(SliceFinderPlottingInterface):
     def __init__(self, sf: "SliceFinder", predict_df: pd.DataFrame, segments: Dict):
         self.sf = sf
         self.df = predict_df
@@ -111,10 +81,20 @@ class SliceFinderPredictFacade(SliceFinderFacade):
     def segment_impact_on_totals(self, s: Dict) -> np.ndarray:
         return s["seg_total_vec"]
 
+    def predict(
+        self,
+        steps: Optional[int] = None,
+        basis: Optional[pd.DataFrame] = None,
+        weight_df: Optional[pd.DataFrame] = None,
+    ):
+        raise NotImplementedError("Can't predict on a prediction")
 
-class TransformedSliceFinder(SliceFinderFacade):
+
+class TransformedSliceFinder(SliceFinderPlottingInterface):
     def __init__(
-        self, sf: "SliceFinder", transformer: Optional[TransformWithWeights] = None
+        self,
+        sf: SliceFinderPlottingInterface,
+        transformer: Optional[TransformWithWeights] = None,
     ):
         # For now, just use log(1+x) as transform, assume sf was fitted on transformed data
         self.sf = sf
@@ -124,21 +104,33 @@ class TransformedSliceFinder(SliceFinderFacade):
             self.tf = transformer
 
         trans_avg = sf.actual_totals / sf.weights  # averages in the transformed space
+        trans_pred_avg = sf.predicted_totals / sf.weights
+
         self.actual_avg = self.tf.inverse_transform_mean(trans_avg)  # a_i
-        self.weights = self.tf.inverse_transform_weight(sf.weights, trans_avg)
-        total = np.sum(self.actual_totals)
-        self.predicted_avg = self.tf.inverse_transform_mean(
-            self.sf.predicted_totals / self.sf.weights
-        )
+        self.predicted_avg = self.tf.inverse_transform_mean(trans_pred_avg)
+
+        # When we do prediction, the corresponding actuals are nans, so need to use predictions
+        # to fill in the gaps for inverse-transforming weights
+        patched_avg = copy.deepcopy(trans_avg)
+        patched_avg[np.isnan(patched_avg)] = trans_pred_avg[np.isnan(patched_avg)]
+        self._weights = self.tf.inverse_transform_weight(sf.weights, patched_avg)
 
         # probably because of some convexity effect of the exp,
         # predictions end up a touch too high on average post-inverse transform
-        self.pred_scaler = total / np.sum(self.predicted_avg * self.weights)
+        # So let's introduce a scaling factor to fix that
+        # Can't use self.predicted_totals here, because it needs self.pred_scaler
+        predicted_totals = self.predicted_avg * self.weights
+        assert not np.isnan(np.sum(predicted_totals))
+
+        nice = ~np.isnan(self.actual_totals)
+        self.pred_scaler = np.sum(self.actual_totals[nice]) / np.sum(
+            predicted_totals[nice]
+        )
 
         # Now let's make sure single-segment impacts add up to total impact
         self.segment_mult = 1.0
 
-        # Try to make indivudial segment  impacts add up to total regression post-transform
+        # Try to make individual segment  impacts add up to total regression post-transform
         # Didn't really make much difference
         # sum_marginals = 0
         # base, _ = self.tf.inverse_transform_totals_weights(self.sf.y_adj, self.sf.weights)
@@ -159,12 +151,16 @@ class TransformedSliceFinder(SliceFinderFacade):
         return self.pred_scaler * self.predicted_avg * self.weights
 
     @property
+    def weights(self):
+        return self._weights
+
+    @property
     def segments(self):
         return self.sf.segments
 
-    @property
-    def y_adj(self):
-        return self.sf.y_adj
+    # @property
+    # def y_adj(self):
+    #     return self.sf.y_adj
 
     @property
     def time(self):
@@ -174,13 +170,13 @@ class TransformedSliceFinder(SliceFinderFacade):
     def total_name(self):
         return self.sf.total_name
 
-    # TODO: cleanly write out the back and forth transforms, with and witout weights
     def segment_impact_on_totals(self, s: Dict) -> np.ndarray:
+        # Calculate transformed prediction with and without the segment
         totals_without_segment = (
             self.sf.predicted_totals - self.sf.segment_impact_on_totals(s)
         )
-        # the base value without any of the coefficients
-        # base, _ = self.tf.inverse_transform_totals_weights(self.sf.y_adj, self.sf.weights)
+
+        # Transform back and subtract
         pt, _ = self.tf.inverse_transform_totals_weights(
             self.sf.predicted_totals, self.sf.weights
         )